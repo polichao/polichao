bdba5edcfeb68a791795bf046ab32933652b8cdc
polichao <kurasarylor@gmail.com>
2022-02-24 17:50:43 +0300

    Some changes in server.php

--- popup-form/lead-catcher/server.php
+++ popup-form/lead-catcher/server.php
@@ -1,22 +1,30 @@
 <?php
-header('Content-Type: application/json');
+//header('Content-Type: application/json');
+header('Content-Type: text/html');
 header('Access-Control-Allow-Origin: *');
 $data = $_POST;
 $name = $data['name'];
+$timezone = new DateTimeZone("+0300");
+
 $tel = $data['tel'];
 if ($data) {
   try{
     $name = isset($name) ? $name : '-';
     $tel = isset($tel) ? $tel : '-';
     $output = "---------------------------------" . "\n";
-    $output .= date("d-m-Y H:i:s")."\n";
+    $output .= (new DateTime())->setTimezone($timezone)->format("d-m-Y H:i:s")."\n";
     $output .= "Имя пользователя: ".$name."\n";
     $output .= "Телефон: ".$tel."\n";
     file_put_contents(dirname(__FILE__).'\logs.txt', $output, FILE_APPEND);
     echo "Заявка в обработке";
   }catch(Exception $e){
+    var_dump($e);
     echo "Error";
   }
 }
+phpinfo();
+
+
+
 
 ?>
